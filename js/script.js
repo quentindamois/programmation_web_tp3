af89c517a4d9ff879660b729e17bd86825689b2c
quentin damois <quentin.damois@efrei.net>
2023-05-16 15:23:31 +0200

    Changes to be committed: 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -37,3 +37,30 @@ function generer() {
     monTableau.appendChild(newLine);
 
 }
+
+/*
+echauffement :
+function generer() {
+    var monFormulaire = document.forms.ajoutPWD;
+    console.log(monFormulaire.nombrecar.value);
+    var newLine = document.ccreateElement("tr");
+    var col1 = document.createElement("td");
+    var col2 = document.createElement("td");
+    var col3 = document.createElement("td");
+    var col4 = document.createElement("td");
+    var col5 = document.createElement("td");
+    col1.textContent = "val1";
+    col2.textContent = "val2";
+    col3.textContent = "val3";
+    col4.textContent = "val4";
+    col5.textContent = "val5";
+    newLine.append(col1);
+    newLine.append(col2);
+    newLine.append(col3);
+    newLine.append(col4);
+    newLine.append(col5);
+    var monTableau = document.getElementById("montab");
+    monTableau.appendChild(newLine);
+
+}
+*/
