5a41d31698d2c665dd270c9a93c5ff7bd1461a7a
quentin damois <quentin.damois@efrei.net>
2023-05-16 15:01:35 +0200

    Changes to be committed: 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 function generer() {
+    var minuscule = "abcdefghijklmnopqrstuvwxyz";
+    var majuscule = "ABCDEFGHIJKLMONPQRSTUVWXYZ"
+    var chiffre = "0123456789";
+    var carspecial = "%!&*^()#$:";
     var monFormulaire = document.forms.ajoutPWD;
     console.log(monFormulaire.nombrecar.value);
+    var password = "";
+    var listecar = "";
+    if (monFormulaire.elements["minuscule"].checked) {
+        listecar = listecar + minuscule;
+    }
+    if (monFormulaire.elements["majuscule"].checked) {
+        listecar = listecar + majuscule;
+    }
+    if (monFormulaire.elements["chiffre"].checked) {
+        listecar = listecar + carspecial;
+    }
+    for (var i  = 1; i <= monFormulaire.elements["nombrecar"].value; i++) {
+        var randomNumber = Math.florr(Math.random() * listecar.lenght);
+        password = password + listecar.substring(randomNumber, randomNumber + 1);
+    }
+    console.log(password);
+    /*
     var newLine = document.ccreateElement("tr");
     var col1 = document.createElement("td");
     var col2 = document.createElement("td");
@@ -19,5 +40,6 @@ function generer() {
     newLine.append(col5);
     var monTableau = document.getElementById("montab");
     monTableau.appendChild(newLine);
+    */
 
 }
