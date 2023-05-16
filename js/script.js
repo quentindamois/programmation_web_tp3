54ed4f1eda813a0f7998dc8b56a04627e24cbd04
quentin damois <quentin.damois@efrei.net>
2023-05-16 15:17:28 +0200

    Changes to be committed: 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -21,25 +21,19 @@ function generer() {
         password = password + listecar.substring(randomNumber, randomNumber + 1);
     }
     console.log(password);
-    /*
     var newLine = document.ccreateElement("tr");
     var col1 = document.createElement("td");
     var col2 = document.createElement("td");
     var col3 = document.createElement("td");
     var col4 = document.createElement("td");
     var col5 = document.createElement("td");
-    col1.textContent = "val1";
-    col2.textContent = "val2";
-    col3.textContent = "val3";
-    col4.textContent = "val4";
-    col5.textContent = "val5";
-    newLine.append(col1);
-    newLine.append(col2);
-    newLine.append(col3);
-    newLine.append(col4);
-    newLine.append(col5);
+    col1.textContent = monFormulaire.elements["nombrecar"].value;
+    col2.textContent = new Date();
+    col3.textContent = monFormulaire.elements["utilisation"].value;
+    col4.textContent = monFormulaire.elements["type-utilisation"].value;
+    col5.textContent = password;
+    newLine.append(col1, col2, col3, col4, col5);
     var monTableau = document.getElementById("montab");
     monTableau.appendChild(newLine);
-    */
 
 }
