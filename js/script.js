ee74384a1a82a81a85bff4af0f33c2beab6490d5
quentin damois <quentin.damois@efrei.net>
2023-05-19 00:28:11 +0200

    Changes to be committed: 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -36,11 +36,7 @@ function generer() {
         var randomNumber = Math.floor(Math.random() * listecar.length);
         password = password + listecar.substring(randomNumber, randomNumber + 1);
     }
-    console.log("minuscule : " + (((password.match(/[a-z]/) == null) && monFormulaire.elements["minuscule"].checked)));
-    console.log("Majuscule : " + (((password.match(/[A-Z]/) == null) && monFormulaire.elements["majuscule"].checked) ));
-    console.log("chiffre : " + (((password.match(/[0-9]/) == null) && monFormulaire.elements["chiffre"].checked) ));
-    console.log("symbole : " + (((password.match(/!|[#-\&]|[\(-\+]|\^|\:/) == null) && monFormulaire.elements["symbole"].checked) ));
-    console.log(password);} while((((password.match(/[a-z]/) == null) && monFormulaire.elements["minuscule"].checked)) || (((password.match(/[A-Z]/) == null) && monFormulaire.elements["majuscule"].checked)) || (((password.match(/[0-9]/) == null) && monFormulaire.elements["chiffre"].checked) ) || (((password.match(/!|[#-\&]|[\(-\+]|\^|:/) == null) && monFormulaire.elements["symbole"].checked)));
+    console.log(password);} while(((password.match(/[a-z]/) == null) && monFormulaire.elements["minuscule"].checked) || ((password.match(/[A-Z]/) == null) && monFormulaire.elements["majuscule"].checked) || ((password.match(/[0-9]/) == null) && monFormulaire.elements["chiffre"].checked) || ((password.match(/!|[#-\&]|[\(-\+]|\^|:/) == null) && monFormulaire.elements["symbole"].checked));
     var newLine = document.createElement("tr");
     var col1 = document.createElement("td");
     var col2 = document.createElement("td");
