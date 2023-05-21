d816aa550ff4c4d0b6d404b5b8a4be95a51b3ebc
quentin damois <quentin.damois@efrei.net>
2023-05-21 16:04:24 +0200

    Changes to be committed: 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,33 @@ var minuscule = "abcdefghijklmnopqrstuvwxyz";
 var majuscule = "ABCDEFGHIJKLMONPQRSTUVWXYZ"
 var chiffre = "0123456789";
 var carspecial = "%!&*^()#$:";
+/*
+partie 2 : Récupérer le contenu du DOM : échauffement
+function generer() {
+    var monFormulaire = document.forms.addPWD;//forms['addPWD'];
+
+    console.log(monFormulaire.number.value);
+    var newLine = document.createElement("tr");
+    var col1 = document.createElement("td");
+    var col2 = document.createElement("td");
+    var col3 = document.createElement("td");
+    var col4 = document.createElement("td");
+    var col5 = document.createElement("td");
+    col1.textContent = "val1";
+    col2.textContent = "val2";
+    col3.textContent = "val3";
+    col4.textContent = "val4";
+    col5.textContent = "val5";
+    newLine.append(col1);
+    newLine.append(col2);
+    newLine.append(col3);
+    newLine.append(col4);
+    newLine.append(col5);
+    var monTableau = document.getElementById("montab");
+    monTableau.appendChild(newLine);
+
+}
+*/
 
 function generer() {
     var monFormulaire = document.forms.ajoutPWD;//forms['addPWD'];
