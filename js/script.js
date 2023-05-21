1d549b7d86aac9e498d57b47d061d26f6bf4b955
quentin damois <quentin.damois@efrei.net>
2023-05-21 16:09:44 +0200

    Changes to be committed: 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -64,6 +64,49 @@ function generer() {
     }
     console.log(password);}
     */
+/*
+Travail à faire : Ajout du mot de passe généré au tableau
+function generer() {
+    
+    var monFormulaire = document.forms.ajoutPWD;//forms['addPWD'];
+    //console.log(monFormulaire.nombre_caractere.value);
+    var password = "";
+    var listecar = "";
+    if (monFormulaire.elements["minuscule"].checked) {
+        listecar = listecar + minuscule;
+    }
+    if (monFormulaire.elements["majuscule"].checked) {
+        listecar = listecar + majuscule;
+    }
+    if (monFormulaire.elements["chiffre"].checked) {
+        listecar = listecar + chiffre;
+    }
+    if (monFormulaire.elements["symbole"].checked) {
+        listecar = listecar + carspecial;
+    }
+    //console.log(listecar.lenght);
+    for (var i  = 1; i <= monFormulaire.elements["number"].value; i++) {
+        var randomNumber = Math.floor(Math.random() * listecar.length);
+        password = password + listecar.substring(randomNumber, randomNumber + 1);
+    }
+    console.log(password);
+
+    var newLine = document.createElement("tr");
+    var col1 = document.createElement("td");
+    var col2 = document.createElement("td");
+    var col3 = document.createElement("td");
+    var col4 = document.createElement("td");
+    var col5 = document.createElement("td");
+    col1.textContent = monFormulaire.elements["number"].value;
+    col2.textContent = monFormulaire.elements["Date de validité"].value;
+    col3.textContent = monFormulaire.elements["monselect"].value;
+    col4.textContent = monFormulaire.elements["Site"].value;
+    col5.textContent = password;
+    newLine.append(col1, col2, col3, col4, col5);
+    var monTableau = document.getElementById("montab");
+    monTableau.appendChild(newLine);}
+*/
+
 
 function generer() {
     var monFormulaire = document.forms.ajoutPWD;//forms['addPWD'];
